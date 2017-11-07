263cf583dc7d8e9c2eac97fce16f49112716a2db
Sunitha Muthukrishna <mksunitha@gmail.com>
2017-11-07 11:59:43 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 if ! [ -e /home/site/wwwroot/index.php -a -e /home/site/wwwroot/sites/default/settings.php ]; then
   echo "Drupal not found, copying to site root"
-  echo "Copying Wordpress files, please refresh in a moment" > /home/site/wwwroot/index.php
+  echo "Copying Drupal files, please refresh in a moment" > /home/site/wwwroot/index.php
   /tmp/copy.sh &
 else
   echo "Existing Drupal installation found"
