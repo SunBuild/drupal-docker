92a89227e577fd84dad13d1385340bf7e2843603
sunitha muthukrishna <sumuth@microsoft.com>
2017-11-08 12:23:29 -0800

    rewriting copy of drupal files

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 FROM appsvcorg/nginx-fpm:0.1
 
-COPY drupal-8.4.2.tar.gz /tmp
+ADD drupal-8.4.2.tar.gz /tmp
 COPY entrypoint.sh /tmp
-COPY copy.sh /tmp
 
 RUN chmod u+x /tmp/entrypoint.sh
-RUN chmod u+x /tmp/copy.sh
 
 ENTRYPOINT ["/tmp/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,10 @@ set -e
 if ! [ -e /home/site/wwwroot/index.php -a -e /home/site/wwwroot/sites/default/settings.php ]; then
   echo "Drupal not found, copying to site root"
   echo "Copying Drupal files, please refresh in a moment" > /home/site/wwwroot/index.php
-  /tmp/copy.sh &
+  tar -xzf /tmp/drupal-8.4.2.tar.gz -C /home/site/wwwroot index.php
+  rm /tmp/drupal-8.4.2.tar.gz 
+  cp -rf C /home/site/wwwroot/drupal-8.4.2/* /home/site/wwwroot
+
 else
   echo "Existing Drupal installation found"
 fi
