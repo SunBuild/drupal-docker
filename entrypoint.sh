50b02826491a7afe01edcd3e2640bf1e92ac4f53
sunitha muthukrishna <sumuth@microsoft.com>
2017-11-08 15:13:24 -0800

    dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,17 +13,9 @@ exec 2>&1
 
 set -x
 
-if [ ! -d "/home/site/wwwroot/docroot" ]; then
-  mkdir -p /home/site/wwwroot/docroot
-fi
-
 drush --version
 
 rm -r /home/site/wwwroot
 cd /home/site
 drush qd wwwroot --profile=minimal 
 
-ssh-keygen -A
-/usr/sbin/sshd
-
-/start.sh
