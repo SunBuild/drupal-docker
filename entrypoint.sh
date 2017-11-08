2c8273350445e9c8b43226f6ddff823bb4e7ed29
Sunitha Muthukrishna <mksunitha@gmail.com>
2017-11-08 13:25:24 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 
 if [ ! -f "/tmp/drupal.tar.gz" ]
 then
-    echo "drupal.tar.gz file not found"  > /home/site/wwwroot/index.php
+    echo "drupal.tar.gz file not found"  
 else
   if ! [ -e /home/site/wwwroot/index.php -a -e /home/site/wwwroot/sites/default/settings.php ]; then
     echo "Drupal not found, copying to site root"
     echo "Copying Drupal files, please refresh in a moment" > /home/site/wwwroot/index.php
-    tar -xzf /tmp/drupal.tar.gz -C /home/site/wwwroot index.php
+    tar -xzf /tmp/drupal.tar.gz -C /home/site/wwwroot
     rm /tmp/drupal.tar.gz 
     cp -rf C /home/site/wwwroot/drupal-8.4.2/* /home/site/wwwroot
 
