b2a16a2857a22a234325b2a6a31480b38faa2434
Sunitha Muthukrishna <mksunitha@gmail.com>
2017-11-08 12:42:42 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,19 @@
 
 set -e
 
-if ! [ -e /home/site/wwwroot/index.php -a -e /home/site/wwwroot/sites/default/settings.php ]; then
-  echo "Drupal not found, copying to site root"
-  echo "Copying Drupal files, please refresh in a moment" > /home/site/wwwroot/index.php
-  tar -xzf /tmp/drupal-8.4.2.tar.gz -C /home/site/wwwroot index.php
-  rm /tmp/drupal-8.4.2.tar.gz 
-  cp -rf C /home/site/wwwroot/drupal-8.4.2/* /home/site/wwwroot
-
+if [ ! -f "/tmp/drupal-8.4.2.tar.gz" ]
+then
+    echo "drupal-8.4.2.tar.gz file not found"  > /home/site/wwwroot/index.php
 else
-  echo "Existing Drupal installation found"
+  if ! [ -e /home/site/wwwroot/index.php -a -e /home/site/wwwroot/sites/default/settings.php ]; then
+    echo "Drupal not found, copying to site root"
+    echo "Copying Drupal files, please refresh in a moment" > /home/site/wwwroot/index.php
+    tar -xzf /tmp/drupal-8.4.2.tar.gz -C /home/site/wwwroot index.php
+    rm /tmp/drupal-8.4.2.tar.gz 
+    cp -rf C /home/site/wwwroot/drupal-8.4.2/* /home/site/wwwroot
+
+  else
+    echo "Existing Drupal installation found"
+  fi
 fi
 
