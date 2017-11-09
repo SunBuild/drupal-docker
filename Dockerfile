a3a6286a23bc0f3244bf922d2389039df3cb75bf
sunitha muthukrishna <sumuth@microsoft.com>
2017-11-09 14:03:54 -0800

    dev

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM appsvcorg/nginx-fpm:0.1
 ENV PATH="/home/site/wwwroot:${PATH}"
 
 COPY entrypoint.sh /tmp
+COPY drupal-8.4.2.tar.gz /tmp 
 
 RUN set -ex \
 	&& apt-get update \
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,10 +14,14 @@ exec 2>&1
 set -x
 
 #Download drupal
-cd /home/site/
-curl -O http://files.drush.org/drush.phar 
-php drush.phar --version
-php drush.phar dl --drupal-project-rename=wwwroot drupal-8.4.2 --yes
+echo "Downloading Drupal CMS on to your container ..Please wait as this can take some time" > /home/site/wwwroot/index.php
+cd /tmp 
+tar -xzvf drupal-8.4.2.tar.gz -C /home/site/wwwroot --strip 1
+
+#cd /home/site/
+#curl -O http://files.drush.org/drush.phar 
+#php drush.phar --version
+#php drush.phar dl --drupal-project-rename=wwwroot drupal-8.4.2 --yes
 
 
 
