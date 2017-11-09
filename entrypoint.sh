a58a93cf18902dcf561e63c5141ad0a22bf6c9c8
Sunitha Muthukrishna <mksunitha@gmail.com>
2017-11-09 14:44:49 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,10 +18,5 @@ echo "Downloading Drupal CMS on to your container ..Please wait as this can take
 cd /tmp 
 tar -xzvf drupal-8.4.2.tar.gz -C /home/site/wwwroot --strip 1
 
-#cd /home/site/
-#curl -O http://files.drush.org/drush.phar 
-#php drush.phar --version
-#php drush.phar dl --drupal-project-rename=wwwroot drupal-8.4.2 --yes
-
 
 
